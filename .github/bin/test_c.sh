6fde0c310ed2be2795a592ca6eb1fe01cf23126a
Nick Wellnhofer <wellnhofer@aevum.de>
2026-08-17 12:30:19 +0200

    ci: Test with ASan and UBSan
    We need -fno-sanitize=function since calling overridden Clownfish
    methods is UB by design.

--- .github/bin/test_c.sh
+++ .github/bin/test_c.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-export INSTALL_DIR="$(pwd)/install"
+set -e
+
+INSTALL_DIR="$(pwd)/install"
 export PATH="$INSTALL_DIR/bin:$PATH"
 export CLOWNFISH_INCLUDE="$INSTALL_DIR/share/clownfish/include"
 export LIBRARY_PATH="$INSTALL_DIR/lib"
 export LD_LIBRARY_PATH="$INSTALL_DIR/lib"
+
+export CC=clang
+export LSAN_OPTIONS=suppressions=../devel/conf/LSan.supp
+export UBSAN_OPTIONS=print_stacktrace=1
 
 git clone --depth=1 https://github.com/lucysearch/lucy-clownfish.git
 
@@ -13,12 +19,29 @@ cd lucy-clownfish/compiler/c
 make
 make install
 
+# charmonizer doesn't allow to specify linker flags, so we patch the
+# Makefile manually.
+
 cd ../../runtime/c
-./configure --prefix=$INSTALL_DIR
+./configure \
+    --prefix=$INSTALL_DIR \
+    -- \
+    -fsanitize=address,undefined \
+    -fno-sanitize=function \
+    -fno-sanitize-recover=all \
+    -fno-omit-frame-pointer
+sed -i -e 's/LDFLAGS = /&-fsanitize=address,undefined /' Makefile
 make
 make install
 
 cd ../../../c
-./configure
+./configure \
+    -- \
+    -D LUCY_UBSAN \
+    -fsanitize=address,undefined \
+    -fno-sanitize=function \
+    -fno-sanitize-recover=all \
+    -fno-omit-frame-pointer
+sed -i -e '/LEMON/! s/LDFLAGS = /&-fsanitize=address,undefined /' Makefile
 make
 make test
